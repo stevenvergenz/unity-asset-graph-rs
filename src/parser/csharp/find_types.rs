fe11a44d3f79925d88a21cf8035ae74b7b32eeea
Steven Vergenz <stvergen@microsoft.com>
2026-01-27 16:25:08 -0800

    Almost done!

--- src/parser/csharp/find_types.rs
+++ src/parser/csharp/find_types.rs
@@ -35,13 +35,19 @@ pub fn find_types(
         })?;
 
     // identify non-nested types, create assets for them
+    let mut asset_map = HashMap::new();
 
     'decls: for (node, name) in &info.type_decl_nodes {
         // walk up the node tree from the type decl
         let mut full_name = name.clone();
-        while let Some(container) = node.parent() {
+        let mut parent = node.clone();
+        while let Some(p) = parent.parent() {
+            // do not record assets for nested types
+            if let Some(_) = info.type_decl_nodes.get(&p) {
+                continue 'decls;
+            }
             // if we find a namespace declaration, add it to the fully-qualified type name
-            if let Some(ns) = info.ns_decl_nodes.get(&container) {
+            else if let Some(ns) = info.ns_decl_nodes.get(&p) {
                 full_name = QualifiedNameRef::try_concat(ns.clone(), full_name)
                     .map_err(|e| ParseError {
                         path: asset.path.as_ref().unwrap().clone(),
@@ -49,20 +55,50 @@ pub fn find_types(
                         inner: Some(Box::new(e)),
                     })?;
             }
-
-            // do not record assets for nested types
-            else if let Some(_) = info.type_decl_nodes.get(&container) {
-                continue 'decls;
-            }
+            parent = p;
         }
 
-        let type_asset = Asset {
+        asset_map.insert(node, Asset {
             id: Id::CsType(full_name.to_owned()),
             asset_type: AssetType::CsType,
             path: None,
             ..Default::default()
-        };
-        def_assets.push(type_asset);
+        });
+    }
+
+    // check all the used types against the declared types, file requests for the mismatches
+    'usages: for (node, name) in info.type_usages {
+        let mut container_asset = None;
+        // walk up the node tree to check context
+        let mut parent = node;
+        while let Some(p) = parent.parent() {
+            // if the type name is locally declared, discard
+            if let Some(scope) = info.type_decl_names.get(&p)
+            && scope.contains(&name) {
+                continue 'usages;
+            }
+            // if the usage is within a top-level type, save it for the broker request
+            container_asset = container_asset.or(asset_map.get_key_value(&p));
+            parent = p;
+        }
+
+        // if the usage was within a defined asset
+        if let Some((decl, asset)) = container_asset {
+            // find all the namespaces in scope
+            let mut ns = HashSet::new();
+            let mut parent = **decl;
+            while let Some(p) = parent.parent() {
+                if let Some(scope) = info.ns_usages.get(&p) {
+                    ns.union(scope);
+                }
+                parent = p;
+            }
+
+            // file request
+            let ns = ns.iter().map(|n| n.to_owned()).collect::<HashSet<QualifiedNameOwned>>();
+            let b = &mut broker.lock().unwrap();
+            b.request(&asset.id, name.to_owned(), ns);
+        }
     }
 
     Ok(())
